0e0c7a3ce1bd589a886b5606a62fd152639787f4
Carlos Campoy <campoy.miro@gmail.com>
2022-03-18 00:05:49 +0100

    2022-03-17

--- 8-kyu.js
+++ 8-kyu.js
@@ -2,3 +2,20 @@
 function opposite(number) {
   return -number;
 }
+
+//2022-03-17
+function solution(str){
+  //turns str into an array (each array element is 1 character from str)
+  let splitStr = str.split('');
+  
+  //reverses the array
+  let reverseSplitStr = splitStr.reverse();
+
+  //joins all elements into a string
+  let joinReversedStr = reverseSplitStr.join('')
+
+  return joinReversedStr;
+
+  //TIP: methods can be stacked
+  //  return str.split('').reverse().join('');
+}
