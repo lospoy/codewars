7d768a992d642061f0faafdd29e475c1a41ebaf5
Carlos Campoy <campoy.miro@gmail.com>
2022-04-17 12:20:53 -0400

    2022-04-17

--- 6-kyu.js
+++ 6-kyu.js
@@ -15,8 +15,7 @@ for (let i = 0; i < number; i++) {
 }
   return sum
 }
-
-//refactored (also less ridiculous)
+//2022-04-16 refactored (also less ridiculous)
 function solution(number){
   let sum = 0;
   
@@ -26,4 +25,25 @@ function solution(number){
     }
   }
   return sum;
+}
+
+//2022-04-17
+function createPhoneNumber(arr){
+  let p1 = String(arr[0]) + String(arr[1]) + String(arr[2])
+  let p2 = String(arr[3]) + String(arr[4]) + String(arr[5])
+  let p3 = String(arr[6]) + String(arr[7]) + String(arr[8]) + String(arr[9])
+  return `(${p1}) ${p2}-${p3}`
+}
+//2022-04-17 loop
+function createPhoneNumber(numbers){
+  let format = "(xxx) xxx-xxxx";
+  for(let i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  return format;
+}
+//2022-04-17 array methods
+function createPhoneNumber(numbers){
+  return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
 }
