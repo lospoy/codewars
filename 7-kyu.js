1541dc32e96be19c563845bfe159c4901c1d3db3
Carlos Campoy <campoy.miro@gmail.com>
2022-05-05 11:28:11 -0400

    2022-05-05

--- 7-kyu.js
+++ 7-kyu.js
@@ -133,3 +133,25 @@ function DNAStrand(dna){
   }
   return str
 }
+
+//2022-05-05
+function isIsogram(str){
+  if (typeof str === undefined){
+    return true
+  }
+  let str2 = str.toLowerCase()
+  let passing = true;
+  let counts = {}
+  for (i = 0; i <= str2.length; i++) {
+    let letter = str2.charAt(i)
+    if(counts[letter]) {
+      counts[letter] = 1 + counts[letter]
+    } else {
+      counts[letter] = 1
+    }
+    if(counts[letter] > 1) {
+      return passing = false
+    }
+  }
+  return passing
+}
