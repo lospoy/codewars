c447110ad8f7018d09caa68b0de983087dfa317e
Carlos Campoy <campoy.miro@gmail.com>
2022-05-12 12:00:36 -0400

    update 6-kyu

--- 6-kyu/6-kyu.js
+++ 6-kyu/6-kyu.js
@@ -1,18 +1,18 @@
+// 6-kyu
 //2022-04-16
 function solution(number){
   let sum = 0
-
-for (let i = 0; i < number; i++) {
-  if (i < 0) {
-    sum = 0
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    sum += i
-  } else if (i % 5 === 0) {
-    sum += i
-  } else if (i % 3 === 0) {
-    sum += i
+  for (let i = 0; i < number; i++) {
+    if (i < 0) {
+      sum = 0
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      sum += i
+    } else if (i % 5 === 0) {
+      sum += i
+    } else if (i % 3 === 0) {
+      sum += i
+    }
   }
-}
   return sum
 }
 //2022-04-16 refactored (also less ridiculous)
@@ -27,6 +27,7 @@ function solution(number){
   return sum;
 }
 
+// 6-kyu
 //2022-04-17
 function createPhoneNumber(arr){
   let p1 = String(arr[0]) + String(arr[1]) + String(arr[2])
@@ -48,6 +49,7 @@ function createPhoneNumber(numbers){
   return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
 }
 
+// 6-kyu
 //2022-04-18
 function likes(names) {
   if(names.length === 0) {return 'no one likes this'}
@@ -68,6 +70,7 @@ function likes(names) {
   }
 }
 
+// 6-kyu
 //2022-04-19
 function findOdd(A) {
   let countNumObj = A.reduce((key, value) => {
@@ -100,8 +103,8 @@ function findOdd(arr) {
   return arr.find((item, index) => arr.filter(el => el == item).length % 2)
 }
 
+// 6-kyu
 //2022-04-20
-//all your arrays are belong to us
 function digital_root(n) {
   let acc = 0
   while(n.toString().split('').length !== 1) {
@@ -110,11 +113,12 @@ function digital_root(n) {
   }
   return n
 }
-//2022-04-20 what makes sense
+//2022-04-20 quick maths
 function digital_root(n) {
   return (n - 1) % 9 + 1;
 }
 
+// 6-kyu
 //2022-04-21
 function spinWords(string){
   return string
@@ -129,14 +133,36 @@ function spinWords(string){
                 .join(' ')
 }
 
+// 6-kyu
 //2022-04-22
 function arrayDiff(a, b) {
   return a.filter(val => !b.includes(val)); //best practice: use 'e' as value variable name
 }
 
+// 6-kyu
 //2022-04-23
 function findOutlier(integers) {
   let even = integers.filter(x => x % 2 === 0)
   let odd = integers.filter(x => x % 2 !== 0)
   return even.length === 1 ? even.shift() : odd.shift() // people used even[0] instead of even.shift()
 }
+
+// 6-kyu
+// Counting Duplicates
+// 2022-05-12
+function duplicateCount(text){
+  if(text == '') {return 0}
+  else {
+    let obj = text
+                .toLowerCase()
+                .split('')
+                .reduce((char, n) => {
+                    if(!char[n]) {
+                      char[n] = 0
+                    }
+                    char[n]++
+                    return char
+                    }, {})
+    return Object.values(obj).filter((x) => x > 1).length
+  }
+} 
