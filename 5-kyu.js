ba43a5a5b7875d9ff5b2bdca805775c430d6cefb
Carlos Campoy <campoy.miro@gmail.com>
2022-05-28 20:57:00 -0400

    2022-05-28

--- 5-kyu.js
+++ 5-kyu.js
@@ -1,7 +1,7 @@
 // ***********************************************************
 // 5-kyu
 // Simple Pig Latin
-//2022-05-26
+// 2022-05-26
 function pigIt(str){
   return str
           .split(" ")
@@ -11,4 +11,22 @@ function pigIt(str){
                     `${word.substr(1)}${word.substr(0,1)}ay` : word
           })
           .join(' ');
+}
+
+// ***********************************************************
+// 5-kyu
+// Moving Zeros To The End
+// 2022-05-28
+function moveZeros(arr) {
+  let count = 0;
+  let n = arr.length
+  for (let i =0 ; i < n ; i++){
+    if(arr[i]!== 0){
+      arr[count++] = arr[i];
+    }
+  }
+    while(count<n) {
+      arr[count++] =0;
+    }
+  return arr
 }
