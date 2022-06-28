902dc212fa5e933c05385912f54ff2213d53835d
Carlos Campoy <campoy.miro@gmail.com>
2022-06-28 16:27:38 -0400

    2022-06-28

--- 5-kyu.js
+++ 5-kyu.js
@@ -92,3 +92,26 @@ function toHex(d) {
     if(d > 255 ) {return "FF";}
     return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
 }
+
+
+// ***********************************************************
+// 5-kyu
+// Calculating with Functions
+// 2022-06-28
+const expression = (n, op) => !op ? n : op(n)
+
+function zero(op) {return expression(0, op)}
+function one(op) {return expression(1, op)}
+function two(op) {return expression(2, op)}
+function three(op) {return expression(3, op)}
+function four(op) {return expression(4, op)}
+function five(op) {return expression(5, op)}
+function six(op) {return expression(6, op)}
+function seven(op) {return expression(7, op)}
+function eight(op) {return expression(8, op)}
+function nine(op) {return expression(9, op)}
+
+function plus(x) {return function(y) {return y + x} }
+function minus(x) {return function(y) {return y - x} }
+function times(x) {return function(y) {return y * x} }
+function dividedBy(x) {return function(y) {return Math.floor(y / x)} }
