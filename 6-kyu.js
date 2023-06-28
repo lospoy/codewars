804fa46dd1e6a18bee1e77caeb4c05ad5dfef8a9
Carlos <36555654+lospoy@users.noreply.github.com>
2023-06-28 17:07:52 -0400

    2023-06-28 alt

--- 6-kyu.js
+++ 6-kyu.js
@@ -629,3 +629,11 @@ function comp(array1, array2) {
 
 	return extractFinalResult(results)
 }
+
+// compact version
+function comp(array1, array2) {
+	if (array1 == null || array2 == null) return false
+	array1.sort((a, b) => a - b)
+	array2.sort((a, b) => a - b)
+	return array1.map((v) => v * v).every((v, i) => v == array2[i])
+}
