56251419a7198ef08b1efae339bd744b2736e97e
Carlos <36555654+lospoy@users.noreply.github.com>
2023-07-12 16:34:05 -0400

    2023-07-12

--- leetCode.js
+++ leetCode.js
@@ -164,3 +164,25 @@ var reverseList = function (head) {
 	head.next = null
 	return res
 }
+
+// 13. Roman to Integer
+// 2023-07-12
+var romanToInt = function (s) {
+	symbols = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000,
+	}
+
+	value = 0
+	for (let i = 0; i < s.length; i += 1) {
+		symbols[s[i]] < symbols[s[i + 1]]
+			? (value -= symbols[s[i]])
+			: (value += symbols[s[i]])
+	}
+	return value
+}
