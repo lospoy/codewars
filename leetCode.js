1de8da646704f6e70bcbf53f82d87f104b56dc5e
Carlos <36555654+lospoy@users.noreply.github.com>
2023-07-12 16:16:38 -0400

    2023-07-12

--- leetCode.js
+++ leetCode.js
@@ -154,3 +154,13 @@ var maxProfit = function (prices) {
 	})
 	return currentMaxProfit
 }
+
+// 206. Reverse Linked List
+// 2023-07-12
+var reverseList = function (head) {
+	if (head == null || head.next == null) return head
+	var res = reverseList(head.next)
+	head.next.next = head
+	head.next = null
+	return res
+}
