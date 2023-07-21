8a5e6d0cc7f7a44b99c453a1d7624af761115414
Carlos <36555654+lospoy@users.noreply.github.com>
2023-07-21 12:12:11 -0400

    2023-07-21

--- leetCode.js
+++ leetCode.js
@@ -245,3 +245,22 @@ var singleNumber = function (nums) {
 	}
 	return uniqNum
 }
+
+// 20. Valid Parentheses
+// 2023-07-21
+var isValid = function (s) {
+	let stack = []
+
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] == '{') {
+			stack.push('}')
+		} else if (s[i] == '[') {
+			stack.push(']')
+		} else if (s[i] == '(') {
+			stack.push(')')
+		} else if (stack.pop() !== s[i]) {
+			return false
+		}
+	}
+	return !stack.length
+}
